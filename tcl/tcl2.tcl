d66b147243bda643861fb6c4f806d16cb253edff
Bhushan A Bangera <73330862+bhushan0512@users.noreply.github.com>
2024-02-19 14:39:51 +0530

    Add files via upload

--- tcl/tcl2.tcl
+++ tcl/tcl2.tcl
@@ -5,31 +5,37 @@ set tf [open out.tr w]
 $ns trace-all $tf
 
 proc finish {} {
-        global ns nf tf
-        $ns flush-trace
-        close $nf
-        close $tf
-        exec nam out.nam &
-        exit 0
+global ns nf tf
+$ns flush-trace
+close $nf
+close $tf
+exec nam out.nam &
+exit(0)
 }
-
 
 set n0 [$ns node]
 set n1 [$ns node]
 set n2 [$ns node]
-$ns duplex-link $n0 $n1 1Mb 10ms DropTail
-$ns duplex-link $n1 $n2 1Mb 5ms DropTail
-$ns queue-limit $n0 $n1 50
+set n3 [$ns node]
+
+$ns duplex-link $n0 $n2 100Mb 10ms DropTail
+$ns duplex-link $n1 $n2 0.001Mb 5ms DropTail
+$ns duplex-link $n2 $n3 0.001Mb 5ms DropTail
+$ns duplex-link $n1 $n3 0.001Mb 5ms DropTail
+
+$ns queue-limit $n0 $n2 50
 $ns queue-limit $n1 $n2 50
+$ns queue-limit $n2 $n3 50
+$ns queue-limit $n1 $n3 50
 
 set udp1 [new Agent/UDP]
 $ns attach-agent $n0 $udp1
 set null [new Agent/Null]
-$ns attach-agent $n2 $null
+$ns attach-agent $n3 $null
 
 $ns connect $udp1 $null
 set cbr1 [new Application/Traffic/CBR]
-$cbr1 set packet-size 500
+$cbr1 set packet-size 200
 $cbr1 set interval 0.005
 $cbr1 attach-agent $udp1
 
@@ -37,3 +43,4 @@ $ns at 0.5 "$cbr1 start"
 $ns at 5.0 "$cbr1 stop"
 $ns at 5.5 "finish"
 $ns run
+
