939942ba4bd90625be126b18382a18f1d1db6e47
Bhushan A Bangera <73330862+bhushan0512@users.noreply.github.com>
2024-02-19 14:40:46 +0530

    Add files via upload

--- c_code/tcl2.c
+++ c_code/tcl2.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
+void main(){
+    FILE *fp = fopen("/home/adminmca/Documents/bhushan/tcl/out.tr", "r");
+    char x = 'd';
+    char c;
+    int count = 0;
 
+    if(!fp)
+        printf("Cant't open a file");
+    
+    while ((c=getc(fp))!=EOF)
+    {
+        if (c == 'd')
+            count++;
+    }
+    printf("Packet droppped %d\n", count);
+    fclose(fp);
+}
